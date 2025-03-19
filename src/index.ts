d245043701179ab18e80573902aeca33a9a47a56
jhotchk2 <jhotchk2@emich.edu>
2025-03-19 18:52:00 -0400

    added supabase connection

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,32 @@
 import express from "express"
 import dotenv from 'dotenv'
 import cors from 'cors'
+import pg from 'pg'
 
 const app = express()
 dotenv.config()
+const { Pool } = pg;
 const PORT = 9090
 
 app.use(express.json())
 app.use(cors())
 
+const pool = new Pool({
+  connectionString: process.env.DB_URL,
+});
+pool.connect();
 
-// To run the server 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
 
 app.get('/', (req, res) => {
   res.status(200).json({message: 'hello'})
+})
+
+app.get('/supabase', async (req, res) => {
+  const { rows } = await pool.query('SELECT * FROM "Profiles"')
+  res.json(rows)
 })
 
 export default app
