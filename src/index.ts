ac7476fef5e03a46f4bbfe8a46c9da3df56913f7
jhotchk2 <jhotchk2@emich.edu>
2025-03-19 18:32:52 -0400

    created basic node server

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+const app = express()
+dotenv.config()
+const PORT = 9090
+
+app.use(express.json())
+app.use(cors())
+
+
+// To run the server 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'hello'})
+})
